cb728cf9b19ebb2cae3d00bfb46d5314ba750252
zhoujiahua <jiahuasir@gmail.com>
2018-02-09 20:40:23 +0800

    锚点功能

---
+++ 前端文件/锚点滚动/scollTop.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    /*锚点滚动特效function封装*/
+    $('a[href*=#],area[href*=#]').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({
+                    scrollTop: targetOffset
+                },
+                1000);
+                //return false;
+            }
+        }
+    });
+});
