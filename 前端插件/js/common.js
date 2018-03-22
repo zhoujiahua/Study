6e62f3d1519e2e5dd65fc09843f67788465f4363
zhoujiahua <jiahuasir@gmail.com>
2018-03-22 14:06:51 +0800

    新增学习内容20180322

--- 前端插件/js/common.js
+++ 前端插件/js/common.js
@@ -1,5 +1,5 @@
 /*公共部分JS*/
 $(function(){
-	$(".header").load("common/header.html");
-	$(".footer").load("common/footer.html");
+	$(".header").load("../common/header.html");
+	$(".footer").load("../common/footer.html");
 })
